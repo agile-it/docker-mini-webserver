f6caf6171d3a8024bb9059154645b643591e902b
Michael Friedrich <Michael.Friedrich@gmx.de>
2019-02-08 12:33:28 +0100

    change routing

--- mini-webserver.js
+++ mini-webserver.js
@@ -4,14 +4,17 @@ var app = express();
 var port = process.env.PORT0 || 3000;
 var host = process.env.HOST || "0.0.0.0";
 
-app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
   if (req.hostname !== "web.piccocloud.de") {
     res.append("Location","https://web.piccocloud.de");
     res.status(302).end();
   }
+  next();
 });
+
+app.use(express.static(__dirname + '/public'));
+
 
 app.use(function(req, res, next) {
   res.status(404).end();
